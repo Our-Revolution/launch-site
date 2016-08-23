0211fe457069f092b7a351d2dbb3de3512795a31
cjmabry <cjmab28@gmail.com>
2016-08-22 19:19:40 -0500

    Fleshed out more pages, candidates data test

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var candidates = require('../data/candidates.json');
 
 /* GET Candidates page. */
 
 router.get('/', function(req, res) {
-  res.render('candidates', { title: 'Candidates' });
+  res.render('candidates', { title: 'Candidates', data: candidates});
 });
 
 module.exports = router;
