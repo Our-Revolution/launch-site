75f7691ec732cb0c1c9bad263141709425520c61
cjmabry <cjmab28@gmail.com>
2016-08-23 12:45:36 -0500

    Fix syntax error in db-setip

--- db-setup.js
+++ db-setup.js
@@ -3,5 +3,5 @@ var connectionString = process.env.DATABASE_URL;
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE subscribers(id SERIAL PRIMARY KEY, email  VARCHAR(255) not null, zip VARCHAR(10)');
+var query = client.query('CREATE TABLE subscribers(id SERIAL PRIMARY KEY, email  VARCHAR(255) not null, zip VARCHAR(10))');
 query.on('end', function() { client.end(); });
