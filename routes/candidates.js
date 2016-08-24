ea1918a15a3a6507edaab550c19776dc7127f00c
cjmabry <cjmab28@gmail.com>
2016-08-23 19:58:19 -0500

    Candidates for days

--- routes/candidates.js
+++ routes/candidates.js
@@ -5,16 +5,22 @@ var candidates = require('../data/candidates.json');
 /* GET Candidates page. */
 
 router.get('/', function(req, res) {
-  res.render('candidates', { title: 'Candidates', data: candidates, active: {candidates: true}});
+  res.render('candidates', { title: 'Our Revolution - Candidates', data: candidates, active: {candidates: true}});
 });
 
 router.get('/:name', function(req, res) {
   name=req.params.name || false;
 
-  if (name) {
-    res.render('candidate', { title: req.params.name, data: candidates, active: {candidates: true}});
+  function isCandidate(value) {
+    if (value in candidates) {
+      return true;
+    }
+  }
+
+  if (isCandidate(name)) {
+    res.render('candidate', { title: "Our Revolution - " + candidates[name].name, data: candidates[name], active: {candidates: true}});
   } else {
-    res.render('candidates', { title: 'Candidates', data: candidates, active: {candidates: true}});
+    res.redirect('/candidates');
   }
 });
 
