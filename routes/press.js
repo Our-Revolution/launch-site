d4ab9dbcb326c6a21223558ec4e23110eeee6926
cjmabry <cjmab28@gmail.com>
2016-08-29 17:25:14 -0500

    Static press pages for now

--- routes/press.js
+++ routes/press.js
@@ -2,11 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET press page. */
-
-router.get('/:article', function(req, res) {
+router.get('/our-revolution-announces-formation-of-board', function(req, res) {
   article = req.params.article;
 
-  res.render('press/' + article, { title: 'Our Revolution', active: {home: true} });
+  res.render('press/our-revolution-announces-formation-of-board', { title: 'Our Revolution', active: {home: true} });
 });
 
 
