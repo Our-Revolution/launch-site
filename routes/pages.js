38f821e62f2f21e8ec4f8a00d66b6c5c448b027a
cjmabry <cjmab28@gmail.com>
2016-08-30 11:36:58 -0500

    Choi and Willis, pollin locators, august 30 candidates

--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* August 20th Candidates */
 router.get('/august-30-primary-candidates', function(req, res) {
 
-  res.render('pages/august-30-primary-candidates', { title: 'Our Revolution _ Help Candidates Win Today', active: {candidates: true} });
+  res.render('pages/august-30-primary-candidates', { title: 'Our Revolution - Help Candidates Win Today', active: {candidates: true}, meta: {
+    description: "You can help five Our Revolution candidates win today."} });
 });
 
 
