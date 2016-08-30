ac569d762bfda1703e439f1e1ec0154de1e9a520
cjmabry <cjmab28@gmail.com>
2016-08-30 11:53:25 -0500

    updated heading

--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* August 20th Candidates */
 router.get('/august-30-primary-candidates', function(req, res) {
 
-  res.render('pages/august-30-primary-candidates', { title: 'Our Revolution - Help Candidates Win Today', active: {candidates: true}, meta: {
+  res.render('pages/august-30-primary-candidates', { title: 'Our Revolution - August 30 Primaries', active: {candidates: true}, meta: {
     description: "Help five of our candidates win today.", url: "https://ourrevolution.com/page/august-30-primary-candidates"} });
 });
 
