3fdffd57f8d422c047877ab01d4a067b906eacc1
Chris Mabry <cjmab28@gmail.com>
2016-09-29 15:19:18 -0500

    Flores feature

--- routes/pages.js
+++ routes/pages.js
@@ -15,4 +15,10 @@ router.get('/candidate-questionnaire', function(req, res) {
     description: "Suggest a campaign or candidate for Our Revolution to endorse.", url: "https://ourrevolution.com/page/candidate-questionnaire"}, layout: "typeform"});
 });
 
+router.get('/join-lucy-flores-support-women-leaders', function(req, res) {
+
+  res.render('pages/join-lucy-flores-support-women-leaders', { title: 'Our Revolution - Women Leaders', meta: {
+    description: "Join Lucy Flores in supporting seven outstanding women leaders.", url: "https://ourrevolution.com/page/join-lucy-flores-support-women-leaders"}});
+});
+
 module.exports = router;
