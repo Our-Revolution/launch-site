aaf8797406e2d1216df53d9daa64a35768ea44b8
Chris Mabry <cjmab28@gmail.com>
2016-09-30 18:32:43 -0500

    New iitiatives structure laid out

--- routes/initiatives.js
+++ routes/initiatives.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var initiatives = require('../data/initiatives.json');
 
 /* GET initiatives page. */
 router.get('/', function(req, res) {
-  res.render('initiatives/index', { title: 'Our Revolution - Ballot Initiatives', active: {initiatives: true} });
+  res.render('initiatives/index', { title: 'Our Revolution - Initiatives', data: initiatives, active: {initiatives: true}});
 });
 
-router.get('/:initiative', function(req, res) {
-  var initiative = req.params.initiative;
+router.get('/:name', function(req, res) {
+  name=req.params.name || false;
 
-  res.render('initiatives/' + initiative, { title: 'Our Revolution - Ballot Initiatives', active: {initiatives: true} });
+  console.log(name);
+
+  function isInitiative(value) {
+    if (value in initiatives) {
+      if (initiatives[value].show) {
+        return true;
+      }
+    }
+  }
+
+  console.log(isInitiative(name));
+
+
+  if (isInitiative(name)) {
+    res.render('initiatives/initiative', { title: "Our Revolution - " + initiatives[name].name, data: initiatives[name], active: {initiatives: true}});
+  } else {
+    res.redirect('/ballot-initiatives');
+  }
 });
+
 
 module.exports = router;
