ab0b5ff4c210177747bbd4f0773ac432d40007cb
Chris Mabry <cjmab28@gmail.com>
2016-10-21 18:06:06 -0500

    Ballot initiative meta images

--- routes/initiatives.js
+++ routes/initiatives.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var initiatives = require('../data/initiatives.json');
+var categories = require('../data/initiative-categories.json');
+var _ = require('lodash');
 
 /* GET initiatives page. */
 router.get('/', function(req, res) {
-  res.render('initiatives/index', { title: 'Our Revolution - Initiatives', data: initiatives, active: {initiatives: true}});
+  var category = req.query.category || false;
+
+  if (_.includes(categories, category)) {
+    var meta = {
+      title: 'Our Revolution - Initiatives', description: "Find a ballot initiative near you to push the political revolution forward.", image:"https://ourrevolution.com/img/meta/" + category + ".png", url: "https://ourrevolution.com/page/ballot-initiatives?" + category
+    }
+  }
+
+  res.render('initiatives/index', { title: 'Our Revolution - Initiatives', data: initiatives, active: {initiatives: true}, meta: meta});
+
 });
 
 router.get('/:name', function(req, res) {
